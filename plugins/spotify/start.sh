ea7a5ac7d21f661ee578aeda799373b1606dfccf
Masafumi KUSHIRO <kuh@KUH-MBA-M4-2025.local>
2026-04-01 22:23:20 +0900

    Fix duplicate --autoplay and add restart loop

--- plugins/spotify/start.sh
+++ plugins/spotify/start.sh
@@ -5,48 +5,35 @@ if [[ -n "$SOUND_DISABLE_SPOTIFY" ]]; then
   exit 0
 fi
 
-# --- ENV VARS ---
-# SOUND_DEVICE_NAME: Set the device broadcast name for Spotify
-# SOUND_SPOTIFY_BITRATE: Set the playback bitrate
 SOUND_DEVICE_NAME=${SOUND_DEVICE_NAME:-"balenaSound Spotify $(echo "$BALENA_DEVICE_UUID" | cut -c -4)"}
 SOUND_SPOTIFY_BITRATE=${SOUND_SPOTIFY_BITRATE:-160}
 
-# SOUND_SPOTIFY_DISABLE_NORMALISATION: Disable volume normalization
 if [[ -z ${SOUND_SPOTIFY_DISABLE_NORMALISATION+x} ]]; then
-  set -- "$@" \
-    --enable-volume-normalisation
+  set -- "$@" --enable-volume-normalisation
 fi
 
-# SOUND_SPOTIFY_USERNAME: Login username for Spotify
-# SOUND_SPOTIFY_PASSWORD: Login password for Spotify
 if [[ -n "$SOUND_SPOTIFY_USERNAME" ]] && [[ -n "$SOUND_SPOTIFY_PASSWORD" ]]; then
-  set -- "$@" \
-    --username "$SOUND_SPOTIFY_USERNAME" \
-    --password "$SOUND_SPOTIFY_PASSWORD"
+  set -- "$@" --username "$SOUND_SPOTIFY_USERNAME" --password "$SOUND_SPOTIFY_PASSWORD"
 fi
 
-# SOUND_SPOTIFY_ENABLE_CACHE: Enable Spotify audio cache
 if [[ -z ${SOUND_SPOTIFY_ENABLE_CACHE+x} ]]; then
-  set -- "$@" \
-    --disable-audio-cache
+  set -- "$@" --disable-audio-cache
 fi
 
-# Start librespot
-# We use set/$@ because librespot for some reason does not like env vars and quote escapes
 echo "Starting Spotify plugin..."
 echo "Device name: $SOUND_DEVICE_NAME"
-[[ -n "$SOUND_SPOTIFY_USERNAME" ]] && [[ -n "$SOUND_SPOTIFY_PASSWORD" ]] && echo "Using provided credentials for Spotify login."
-[[ -n ${SOUND_SPOTIFY_DISABLE_NORMALISATION+x} ]] && echo "Volume normalization disabled."
-[[ -n ${SOUND_SPOTIFY_ENABLE_CACHE+x} ]] && echo "Spotify audio cache enabled."
 
-set -- /usr/bin/librespot \
-  --backend pulseaudio \
-  --name "$SOUND_DEVICE_NAME" \
-  --bitrate "$SOUND_SPOTIFY_BITRATE" \
-  --cache /var/cache/raspotify \
-  --volume-ctrl linear \
-  --autoplay \
-  --autoplay \
-  "$@"
+CMD=(/usr/bin/librespot
+  --backend pulseaudio
+  --name "$SOUND_DEVICE_NAME"
+  --bitrate "$SOUND_SPOTIFY_BITRATE"
+  --cache /var/cache/raspotify
+  --volume-ctrl linear
+  --autoplay
+  "$@")
 
-exec "$@"
+while true; do
+  "${CMD[@]}"
+  echo "librespot exited, restarting in 2 seconds..."
+  sleep 2
+done
